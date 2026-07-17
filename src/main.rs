7c0031114e2a158d4a25ff74dad205e34d176b73
makoto-tsudo <117238120+makoto-tsudo@users.noreply.github.com>
2026-07-17 19:59:39 +0900

    bumpup quantum-lt to v0.3.0

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,11 @@ enum SubCommands {
         #[arg(short, long)]
         serial: String,
     },
+    #[clap(arg_required_else_help = true)]
+    Query {
+        #[arg(short, long)]
+        serial: String,
+    },
     List,
 }
 
@@ -29,6 +34,30 @@ fn main() -> Result<(), Error> {
             if let Some(mut ctx) = ctx {
                 quantum_lt::init(&mut ctx)?;
                 println!("Success initialization.")
+            } else {
+                eprintln!("No such device. {}", serial)
+            }
+        },
+        SubCommands::Query { serial } => {
+            let ctx = quantum_lt::search(Some(&serial))?
+                .into_iter()
+                .next();
+            if let Some(mut ctx) = ctx {
+                let res = quantum_lt::query(&mut ctx)?;
+                for i in 0 .. 16 {
+                    println!("in  {:2}: {:.3}", i + 1, res.input[i]);
+                }
+                for i in 0 .. 10 {
+                    println!("daw {:2}: {:.3}", i + 1, res.daw[i]);
+                }
+                for i in 0 .. 10 {
+                    println!("out {:2}: {:.3}", i + 1, res.output[i]);
+                }
+                println!("Opt1: 0x{:02x}", res.opt1);
+                println!("Opt2: 0x{:02x}", res.opt2);
+                let version = str::from_utf8(&res.version)
+                    .map_err(|_| Error::PacketParse)?;
+                println!("Version: {}", version);
             } else {
                 eprintln!("No such device. {}", serial)
             }
